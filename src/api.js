4b39e39dd356e8feb885567132c58dede23846c9
Kateryna Tkachenko <Tkachenko.city@gmail.com>
2023-12-05 13:53:02 +0200

    fix onSubmit

--- src/api.js
+++ src/api.js
@@ -4,21 +4,9 @@ const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '32131085-77c33ae4af62fbdfe36accafe';
 
 async function servicePixabay(searchQuery, page) {
-  // const params = new URLSearchParams({
-  //   key: API_KEY,
-  //   q: searchQuery,
-  //   image_type: 'photo',
-  //   orientation: 'horizontal',
-  //   safesearch: true,
-  //   page,
-  //   per_page: 40,
-  // });
-
   return await axios.get(
-    `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=3`
+    `${BASE_URL}?key=${API_KEY}&q=${searchQuery}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`
   );
-  //   console.log(data);
-  //   console.log(data.hits);
 }
 
 export { servicePixabay };
--- src/index.js
+++ src/index.js
@@ -13,26 +13,72 @@ const elements = {
 elements.form.addEventListener('submit', onSubmit);
 // elements.btnLoadMore.addEventListener('click', onLoadMore);
 
+elements.btnLoadMore.style.display = 'none';
+
 let page = 1;
+let currentPage;
+const per_page = 40;
 
 async function onSubmit(event) {
   event.preventDefault();
   elements.gallery.innerHTML = '';
+
   page = 1;
   const formData = new FormData(event.currentTarget);
   const searchQuery = formData.get('searchQuery');
-  console.log(searchQuery);
+  console.log('searchQuery', searchQuery);
 
   try {
     const { data } = await servicePixabay(searchQuery, page);
     console.log('data', data);
+    elements.gallery.insertAdjacentHTML('beforeend', createGallery(data.hits));
+
+    if (data.totalHits > per_page) {
+      elements.btnLoadMore.style.display = 'block';
+      let totalPages = data.totalHits / per_page;
+      console.log(`Total pages = ${Math.ceil(totalPages)}`);
+    } else if (data.totalHits < per_page) {
+      elements.btnLoadMore.style.display = 'none';
+      console.log('This is the last page of Gallery');
+    }
   } catch (error) {
     console.log(error);
+    console.log(error.message);
   }
 }
 
-// elements.btnLoadMore.style.display = 'none';
-// let page = 1;
+function createGallery(arr) {
+  return arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+        <img src="${webformatURL}" alt="${tags}" loading="lazy" width="150"/>
+        <div class="info">
+            <p class="info-item">
+            <b>Likes: ${likes}</b>
+            </p>
+            <p class="info-item">
+            <b>Views: ${views}</b>
+            </p>
+            <p class="info-item">
+            <b>Comments: ${comments}</b>
+            </p>
+            <p class="info-item">
+            <b>Downloads: ${downloads}</b>
+            </p>
+        </div>
+    </div>
+    `
+    )
+    .join('');
+}
 
 // async function onLoadMore() {
 //   const searchQuery = elements.form.elements.searchQuery.value;
@@ -57,37 +103,4 @@ async function onSubmit(event) {
 //   } catch (error) {
 //     console.error('Error loading more data:', error);
 //   }
-// }
-
-// function renderGallery(arr) {
-//   return arr
-//     .map(
-//       ({
-//         webformatURL,
-//         largeImageURL,
-//         tags,
-//         likes,
-//         views,
-//         comments,
-//         downloads,
-//       }) => `<div class="photo-card">
-//         <img src="${webformatURL}" alt="${tags}" loading="lazy" width="150"/>
-//         <div class="info">
-//             <p class="info-item">
-//             <b>Likes: ${likes}</b>
-//             </p>
-//             <p class="info-item">
-//             <b>Views: ${views}</b>
-//             </p>
-//             <p class="info-item">
-//             <b>Comments: ${comments}</b>
-//             </p>
-//             <p class="info-item">
-//             <b>Downloads: ${downloads}</b>
-//             </p>
-//         </div>
-//     </div>
-//     `
-//     )
-//     .join('');
 // }
