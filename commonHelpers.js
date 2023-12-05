05d59884c8650f4b3e42a4c596373178f0399cbf
TkachenkoKaterina <90516132+TkachenkoKaterina@users.noreply.github.com>
2023-12-05 11:53:27 +0000

    Deploying to gh-pages from @ TkachenkoKaterina/goit-advancedjs-hw-04@4b39e39dd356e8feb885567132c58dede23846c9 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,19 @@
-import{a as l}from"./assets/vendor-e79added.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const c of t.addedNodes)c.tagName==="LINK"&&c.rel==="modulepreload"&&r(c)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function r(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const u="https://pixabay.com/api/",f="32131085-77c33ae4af62fbdfe36accafe";async function d(n,o){return await l.get(`${u}?key=${f}&q=${n}&image_type=photo&orientation=horizontal&safesearch=true&page=${o}&per_page=3`)}const i={form:document.querySelector("#search-form"),btnSearch:document.querySelector("button[type = submit]"),gallery:document.querySelector(".gallery"),btnLoadMore:document.querySelector(".load-more")};i.form.addEventListener("submit",m);let s=1;async function m(n){n.preventDefault(),i.gallery.innerHTML="",s=1;const a=new FormData(n.currentTarget).get("searchQuery");console.log(a);try{const{data:r}=await d(a,s);console.log("data",r)}catch(r){console.log(r)}}
+import{a as f}from"./assets/vendor-e79added.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const l of o.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&t(l)}).observe(document,{childList:!0,subtree:!0});function s(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerpolicy&&(o.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?o.credentials="include":e.crossorigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function t(e){if(e.ep)return;e.ep=!0;const o=s(e);fetch(e.href,o)}})();const u="https://pixabay.com/api/",m="32131085-77c33ae4af62fbdfe36accafe";async function p(a,r){return await f.get(`${u}?key=${m}&q=${a}&image_type=photo&orientation=horizontal&safesearch=true&page=${r}&per_page=40`)}const n={form:document.querySelector("#search-form"),btnSearch:document.querySelector("button[type = submit]"),gallery:document.querySelector(".gallery"),btnLoadMore:document.querySelector(".load-more")};n.form.addEventListener("submit",y);n.btnLoadMore.style.display="none";let c=1;const i=40;async function y(a){a.preventDefault(),n.gallery.innerHTML="",c=1;const s=new FormData(a.currentTarget).get("searchQuery");console.log("searchQuery",s);try{const{data:t}=await p(s,c);if(console.log("data",t),n.gallery.insertAdjacentHTML("beforeend",g(t.hits)),t.totalHits>i){n.btnLoadMore.style.display="block";let e=t.totalHits/i;console.log(`Total pages = ${Math.ceil(e)}`)}else t.totalHits<i&&(n.btnLoadMore.style.display="none",console.log("This is the last page of Gallery"))}catch(t){console.log(t),console.log(t.message)}}function g(a){return a.map(({webformatURL:r,largeImageURL:s,tags:t,likes:e,views:o,comments:l,downloads:d})=>`<div class="photo-card">
+        <img src="${r}" alt="${t}" loading="lazy" width="150"/>
+        <div class="info">
+            <p class="info-item">
+            <b>Likes: ${e}</b>
+            </p>
+            <p class="info-item">
+            <b>Views: ${o}</b>
+            </p>
+            <p class="info-item">
+            <b>Comments: ${l}</b>
+            </p>
+            <p class="info-item">
+            <b>Downloads: ${d}</b>
+            </p>
+        </div>
+    </div>
+    `).join("")}
 //# sourceMappingURL=commonHelpers.js.map
